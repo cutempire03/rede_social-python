b7005af111d59d6072d373907f45b2fc50237656
Gustavo Toledo <gustavotoledor07@gmail.com>
2026-04-30 00:28:28 -0300

    a

--- src/api/application.py
+++ src/api/application.py
@@ -1,15 +1,11 @@
 from fastapi import FastAPI
 from src.api.configuration import configure_db, configure_routes
 
-app = FastAPI()
-
 def create_app():
     app = FastAPI()
 
-    # iniciar db/tortoise
     configure_routes(app)
-    configure_db(app)
-    
+    # configure_db(app)
 
     return app
 
